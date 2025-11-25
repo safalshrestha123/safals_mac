85cb7ea764775c6a25fb0095a725cbd50a300cca
safalstha1 <safal.shrestha@globalyhub.com>
2025-11-25 13:51:06 -0500

    welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from "./components/navbar.jsx";
+import {Navbar, Welcome,} from "#components/index.js";
 
 const App = () => {
     return (
         <main>
             <Navbar />
+            <Welcome />
         </main>
     )
 }
